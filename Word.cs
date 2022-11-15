37ae322498b2eacd9563d12b6a0db8dd281673a4
cab64652 <cabc11918@gmail.com>
2022-11-15 09:22:50 -0700

    Updated Word to add "_" for the letters in _secret

--- Word.cs
+++ Word.cs
@@ -1,25 +1,35 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Hangman
 {
     public class Word
     {
         // These are the attributes
-        private List<string> _secret = new List<string>() {"b","a","n","a","n","a"};
-        private List<string> _guesses = new List<string>() { "_", "_", "_", "_", "_", "_"};
+        // private List<string> _secret = new List<string>() {"b","a","n","a","n","a"};
+        // private List<string> _guesses = new List<string>() { "_", "_", "_", "_", "_", "_"};
+
+        private List<string> _secret = new List<string>();
+        private List<string> _guesses = new List<string>();
 
         // This is the constructor.
         public Word()
         {
             List<string> words = new List<string>() { "banana", "apple", "orange"};
             Random random = new Random();
-            int index = random.Next(0,2);
+            int index = random.Next(words.Count);
             string word = words[index];
-            _secret = new List<string>(word.Split());
+            string[] letters = word.ToCharArray().Select(c => c.ToString()).ToArray();
+            _secret = new List<string>(letters);
+            // _secret = new List<string>(word.Split(""));
 
             // use a loop to set _guesses to a list of underscores (the same number of letters in _secret)
         
+            for (int i = 0; i < _secret.Count; i++)
+            {
+                _guesses.Add("_");
+            }
         
         }
 
